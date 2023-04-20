09f4f0059a12a3c0910c9c4ec0f99fc41abce9fa
Jaimi Patel <jaimip34@gmail.com>
2023-04-20 16:43:05 +0100

    add scripts to build and run vm

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if ! [[ "$0" =~ scripts/build.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+# Set default binary directory location
+name="kneDvWXFFZ68XzNjnLftBaJ4xARmSPVC4neR8dyR8ERYiKFfe"
+
+# Build blobvm, which is run as a subprocess
+mkdir -p ./build
+
+echo "Building clobvm in ./build/$name"
+go build -o ./build/$name ./cmd/clobvm
+
---
+++ scripts/run.sh
@@ -0,0 +1,169 @@
+#!/usr/bin/env bash
+set -e
+
+# e.g.,
+# ./scripts/run.sh 1.7.13
+#
+# run without e2e tests
+# ./scripts/run.sh 1.7.13
+#
+# to run E2E tests (terminates cluster afterwards)
+# E2E=true ./scripts/run.sh 1.7.13
+if ! [[ "$0" =~ scripts/run.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+VERSION=$1
+if [[ -z "${VERSION}" ]]; then
+  echo "Missing version argument!"
+  echo "Usage: ${0} [VERSION]" >> /dev/stderr
+  exit 255
+fi
+
+MODE=${MODE:-run}
+E2E=${E2E:-false}
+if [[ ${E2E} == true ]]; then
+  MODE="test"
+fi
+
+AVALANCHE_LOG_LEVEL=${AVALANCHE_LOG_LEVEL:-INFO}
+
+echo "Running with:"
+echo VERSION: ${VERSION}
+echo MODE: ${MODE}
+
+CLOBVM_PATH=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  cd .. && pwd
+)
+source "$CLOBVM_PATH"/scripts/constants.sh
+
+############################
+# download avalanchego
+# https://github.com/ava-labs/avalanchego/releases
+GOARCH=$(go env GOARCH)
+GOOS=$(go env GOOS)
+DOWNLOAD_URL=https://github.com/ava-labs/avalanchego/releases/download/v${VERSION}/avalanchego-linux-${GOARCH}-v${VERSION}.tar.gz
+DOWNLOAD_PATH=/tmp/avalanchego.tar.gz
+if [[ ${GOOS} == "darwin" ]]; then
+  DOWNLOAD_URL=https://github.com/ava-labs/avalanchego/releases/download/v${VERSION}/avalanchego-macos-v${VERSION}.zip
+  DOWNLOAD_PATH=/tmp/avalanchego.zip
+fi
+
+rm -rf /tmp/avalanchego-v${VERSION}
+rm -rf /tmp/avalanchego-build
+rm -f ${DOWNLOAD_PATH}
+
+echo "downloading avalanchego ${VERSION} at ${DOWNLOAD_URL}"
+curl -L ${DOWNLOAD_URL} -o ${DOWNLOAD_PATH}
+
+echo "extracting downloaded avalanchego"
+if [[ ${GOOS} == "linux" ]]; then
+  tar xzvf ${DOWNLOAD_PATH} -C /tmp
+elif [[ ${GOOS} == "darwin" ]]; then
+  unzip ${DOWNLOAD_PATH} -d /tmp/avalanchego-build
+  mv /tmp/avalanchego-build/build /tmp/avalanchego-v${VERSION}
+fi
+find /tmp/avalanchego-v${VERSION}
+
+AVALANCHEGO_PATH=/tmp/avalanchego-v${VERSION}/avalanchego
+AVALANCHEGO_PLUGIN_DIR=/tmp/avalanchego-v${VERSION}/plugins
+
+############################
+
+############################
+echo "building clobvm"
+go build \
+-o /tmp/avalanchego-v${VERSION}/plugins/kneDvWXFFZ68XzNjnLftBaJ4xARmSPVC4neR8dyR8ERYiKFfe \
+./cmd/clobvm
+find /tmp/avalanchego-v${VERSION}
+############################
+
+############################
+
+echo "creating genesis file"
+echo -n "e2e" >/tmp/.clobvm.genesis
+
+############################
+
+############################
+echo "building e2e.test"
+# to install the ginkgo binary (required for test build and run)
+go install -v github.com/onsi/ginkgo/v2/ginkgo@v2.1.4
+ACK_GINKGO_RC=true ginkgo build ./tests/e2e
+./tests/e2e/e2e.test --help
+
+#################################
+# download avalanche-network-runner
+# https://github.com/ava-labs/avalanche-network-runner
+# TODO: use "go install -v github.com/ava-labs/avalanche-network-runner/cmd/avalanche-network-runner@v${NETWORK_RUNNER_VERSION}"
+NETWORK_RUNNER_VERSION=1.4.1
+DOWNLOAD_PATH=/tmp/avalanche-network-runner.tar.gz
+DOWNLOAD_URL=https://github.com/ava-labs/avalanche-network-runner/releases/download/v${NETWORK_RUNNER_VERSION}/avalanche-network-runner_${NETWORK_RUNNER_VERSION}_linux_amd64.tar.gz
+if [[ ${GOOS} == "darwin" ]]; then
+  DOWNLOAD_URL=https://github.com/ava-labs/avalanche-network-runner/releases/download/v${NETWORK_RUNNER_VERSION}/avalanche-network-runner_${NETWORK_RUNNER_VERSION}_darwin_amd64.tar.gz
+fi
+
+rm -f ${DOWNLOAD_PATH}
+rm -f /tmp/avalanche-network-runner
+
+echo "downloading avalanche-network-runner ${NETWORK_RUNNER_VERSION} at ${DOWNLOAD_URL}"
+curl -L ${DOWNLOAD_URL} -o ${DOWNLOAD_PATH}
+
+echo "extracting downloaded avalanche-network-runner"
+tar xzvf ${DOWNLOAD_PATH} -C /tmp
+/tmp/avalanche-network-runner -h
+
+############################
+# run "avalanche-network-runner" server
+echo "launch avalanche-network-runner in the background"
+/tmp/avalanche-network-runner \
+server \
+--log-level debug \
+--port=":12342" \
+--disable-grpc-gateway &
+PID=${!}
+
+############################
+# By default, it runs all e2e test cases!
+# Use "--ginkgo.skip" to skip tests.
+# Use "--ginkgo.focus" to select tests.
+# echo "running e2e tests"
+./tests/e2e/e2e.test \
+--ginkgo.v \
+--network-runner-log-level debug \
+--network-runner-grpc-endpoint="0.0.0.0:12342" \
+--avalanchego-path=${AVALANCHEGO_PATH} \
+--avalanchego-plugin-dir=${AVALANCHEGO_PLUGIN_DIR} \
+--vm-genesis-path=/tmp/.clobvm.genesis \
+--output-path=/tmp/avalanchego-v${VERSION}/output.yaml \
+--mode=${MODE}
+
+############################
+# e.g., print out MetaMask endpoints
+if [[ -f "/tmp/avalanchego-v${VERSION}/output.yaml" ]]; then
+  echo "cluster is ready!"
+  cat /tmp/avalanchego-v${VERSION}/output.yaml
+else
+  echo "cluster is not ready in time... terminating ${PID}"
+  kill ${PID}
+  exit 255
+fi
+
+############################
+if [[ ${MODE} == "test" ]]; then
+  # "e2e.test" already terminates the cluster for "test" mode
+  # just in case tests are aborted, manually terminate them again
+  echo "network-runner RPC server was running on PID ${PID} as test mode; terminating the process..."
+  pkill -P ${PID} || true
+  kill -2 ${PID}
+  pkill -9 -f kneDvWXFFZ68XzNjnLftBaJ4xARmSPVC4neR8dyR8ERYiKFfe || true # in case pkill didn't work
+else
+  echo "network-runner RPC server is running on PID ${PID}..."
+  echo ""
+  echo "use the following command to terminate:"
+  echo ""
+  echo "pkill -P ${PID} && kill -2 ${PID} && pkill -9 -f kneDvWXFFZ68XzNjnLftBaJ4xARmSPVC4neR8dyR8ERYiKFfe"
+  echo ""
+fi
