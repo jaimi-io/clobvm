ca433b9126db7388f9cdb11bea986d85d84e8fb5
Jaimi Patel <jaimip34@gmail.com>
2023-05-29 20:24:37 +0100

    implement token transfer tx type

--- actions/transfer.go
+++ actions/transfer.go
@@ -2,13 +2,21 @@ package actions
 
 import (
 	"context"
+	"errors"
 
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/jaimi-io/clobvm/auth"
+	"github.com/jaimi-io/clobvm/storage"
 	"github.com/jaimi-io/hypersdk/chain"
 	"github.com/jaimi-io/hypersdk/codec"
+	"github.com/jaimi-io/hypersdk/crypto"
+	"github.com/jaimi-io/hypersdk/utils"
 )
 
 type Transfer struct {
+	To crypto.PublicKey
+	TokenID ids.ID
+	Amount uint64
 }
 
 func (t *Transfer) MaxUnits(r chain.Rules) uint64 {
@@ -16,15 +24,19 @@ func (t *Transfer) MaxUnits(r chain.Rules) uint64 {
 }
 
 func (t *Transfer) ValidRange(r chain.Rules) (start int64, end int64) {
-	return 0, 0
+	return -1, -1
 }
 
-func (t *Transfer) StateKeys() [][]byte {
-	return nil
+func (t *Transfer) StateKeys(cauth chain.Auth, _ ids.ID) [][]byte {
+	user := auth.GetUser(cauth)
+	return [][]byte{
+		storage.BalanceKey(user, t.TokenID),
+		storage.BalanceKey(t.To, t.TokenID),
+	}
 }
 
 func (t *Transfer) Fee() (amount int64, tokenID ids.ID) {
-	return 0, ids.ID{}
+	return 0, t.TokenID
 }
 
 func (t *Transfer) Execute(
@@ -32,13 +44,26 @@ func (t *Transfer) Execute(
 	r chain.Rules,
 	db chain.Database,
 	timestamp int64,
-	auth chain.Auth,
+	cauth chain.Auth,
 	txID ids.ID,
 	warpVerified bool,
 ) (result *chain.Result, err error) {
-	return nil, nil
+	user := auth.GetUser(cauth)
+	if t.Amount == 0 {
+		err = errors.New("amount cannot be zero")
+		return &chain.Result{Success: false, Units: 0, Output: utils.ErrBytes(err)}, err
+	}
+	if err = storage.DecBalance(ctx, db, user, t.TokenID, t.Amount); err != nil {
+		return &chain.Result{Success: false, Units: 0, Output: utils.ErrBytes(err)}, err
+	}
+	if err = storage.IncBalance(ctx, db, t.To, t.TokenID, t.Amount); err != nil {
+		return &chain.Result{Success: false, Units: 0, Output: utils.ErrBytes(err)}, err
+	}
+	return &chain.Result{Success: true, Units: 0}, nil
 }
 
 func (t *Transfer) Marshal(p *codec.Packer) {
-
+	p.PackPublicKey(t.To)
+	p.PackID(t.TokenID)
+	p.PackUint64(t.Amount)
 }
