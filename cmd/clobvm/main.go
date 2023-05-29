9792e621a1b10ad0410abfd64a23befb138c1b3f
Jaimi Patel <jaimip34@gmail.com>
2023-05-29 20:27:02 +0100

    add clobvm cmd for compilation

---
+++ cmd/clobvm/main.go
@@ -0,0 +1,48 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/utils/ulimit"
+	"github.com/ava-labs/avalanchego/vms/rpcchainvm"
+	"github.com/jaimi-io/clobvm/controller"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:        "clobvm",
+	Short:      "ClobVM agent",
+	SuggestFor: []string{"clobvm"},
+	RunE:       runFunc,
+}
+
+func init() {
+	cobra.EnablePrefixMatching = true
+}
+
+// func init() {
+// 	rootCmd.AddCommand(
+// 		version.NewCommand(),
+// 	)
+// }
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "tokenvm failed %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func runFunc(*cobra.Command, []string) error {
+	if err := ulimit.Set(ulimit.DefaultFDLimit, logging.NoLog{}); err != nil {
+		return fmt.Errorf("%w: failed to set fd limit correctly", err)
+	}
+	return rpcchainvm.Serve(context.TODO(), controller.New())
+}
