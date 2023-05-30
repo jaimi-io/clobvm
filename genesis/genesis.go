ebb7cec546c404a829c340439e1cf487ed1194cb
Jaimi Patel <jaimip34@gmail.com>
2023-05-30 02:53:10 +0100

    set token balances in genesis

--- genesis/genesis.go
+++ genesis/genesis.go
@@ -3,9 +3,12 @@ package genesis
 import (
 	"context"
 
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/trace"
 
+	"github.com/jaimi-io/clobvm/storage"
 	"github.com/jaimi-io/hypersdk/chain"
+	"github.com/jaimi-io/hypersdk/crypto"
 )
 
 type Genesis struct {}
@@ -22,6 +25,15 @@ func (g *Genesis) Load(ctx context.Context, tracer trace.Tracer, db chain.Databa
 	ctx, span := tracer.Start(ctx, "genesis.Load")
 	defer span.End()
 
-	// TODO: add token amounts
-	return nil
+	addr, err := crypto.ParseAddress("clob", "clob1wve8exfmxhzfd2y0jl4aaqyg5yyj3z64k6v5fpe75ck7h7lkg5rscxxzlz")
+	if err != nil {
+		return err
+	}
+	c := make([]byte, 32)
+	copy(c, []byte("USDC"))
+	id, err := ids.ToID(c)
+	if err != nil {
+		return err
+	}
+	return storage.SetBalance(ctx, db, addr, id, 1000000000000)
 }
